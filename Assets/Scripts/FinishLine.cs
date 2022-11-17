8a3a61942afb93afcc022d910a675ae54187908f
Zaida Kodzoeva <zkod777@gmail.com>
2022-11-17 15:49:29 +0400

    first commit

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement; // adding this namespace to start using scene management and changing scenes
+using UnityEngine;
+ 
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+ 
+    public void OnCollisionEnter2D(Collision2D other){
+        if (other.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene(SceneManager.sceneCount + 1);
+        }
+    }
+}
+ 
