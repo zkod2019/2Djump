bf8e36084b79f23291eadac65d6d6b4c794361da
Zaida Kodzoeva <zkod777@gmail.com>
2022-11-18 15:58:11 +0400

    attempt 2 at fixing enemy fall script for level 3 enemy'

--- Assets/Scripts/FallEnemy.cs
+++ Assets/Scripts/FallEnemy.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class FallEnemy : MonoBehaviour
 {
-    int x = -7;
-    int y = 2;
-    public GameObject startPoint;
-    public GameObject Player;
+    int x = -20;
+    int y = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +19,13 @@ public class FallEnemy : MonoBehaviour
     }
    // static Vector2 startPosition = new Vector2(20, 20);
     private void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.CompareTag("Player")){
-            Player.transform.position = startPoint.transform.position;
-        }
-         if (other.gameObject.CompareTag("Floor")){
+        if (other.gameObject.CompareTag("Floor")){
             Debug.Log("wefg");
-            Player.transform.position = new Vector2(transform.position.x, transform.position.y);//startPosition.transform.position;
+            other.transform.position = new Vector2(transform.position.x, transform.position.y);//startPosition.transform.position;
             // - 20 20 x
             // y 25  transform.position2D() 
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other){
-       if (other.gameObject.CompareTag("Floor")){
-            Debug.Log("wefg");
-            Player.transform.position = new Vector2(transform.position.x, transform.position.y);//startPosition.transform.position;
-            // - 20 20 x
-            // y 25  transform.position2D() 
-        }
-    }
+   
 }
