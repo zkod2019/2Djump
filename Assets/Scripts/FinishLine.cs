68b38b4d2ea32d3c028a916c92625234491506c0
Zaida Kodzoeva <zkod777@gmail.com>
2022-11-21 15:54:13 +0400

    attempt 2 at level counter

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement; // adding this namespace to start using scene management and changing scenes
 using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
  
 public class FinishLine : MonoBehaviour
 {
-    public int count = 0;
+    public Text levelText;
+    public static int levelCount = 1;
+    private string test;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +25,21 @@ public class FinishLine : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Player")){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // .sceneCount + 1);
-            count = count + 1;
-            Debug.Log(count);
+            
+            levelText=GameObject.Find("LevelCount").GetComponent<Text>(); 
+            Debug.Log(levelText);
+
+           // levelText.text = levelCount.ToString();
+            //levelCount = (int)((JObject)fields.GetValue("level")).GetValue("integerValue");
+
+            //numericalScore.GetComponent<Text>().text = "Score : " + scoreManager.score; 
+            levelCount = levelCount + 1;
+            levelText.text =  "Loop: " + levelCount.ToString();
+            
+
+            Debug.Log("Timer initialized!!");
+            Debug.Log(levelCount);
+            
         }
     }
 }
